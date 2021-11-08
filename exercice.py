b76afe0cca72b39bd20c1ff386501bdbcda54dd6
A123wq123 <charles.degrandpre@polymtl.ca>
2021-11-08 09:40:24 -0500

    exercices chap 8 version 1 (deux premeir exercices tentés)

--- exercice.py
+++ exercice.py
@@ -6,7 +6,43 @@ PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80
 # TODO: Importez vos modules ici
 
 
+
+
 # TODO: Définissez vos fonction ici
+def comparateur_de_deux_fichiers(fichier_1, fichier_2):
+    with open(fichier_1, "r") as f1:
+        f1_read = f1.read()
+
+    with open(fichier_2, "r") as f2:
+        f2_read = f2.read()
+
+    for i, y in zip(f1_read, f2_read):
+
+        if i != y:
+            return (f"Première différence rencontrée: {i} et {y}")
+
+    return("Aucune différence encontrée")
+
+def recopiage(fichier):
+
+    with open(fichier, "r") as f1:
+        f1_read = f1.read()
+
+    with open("fichier_copie_espaces", "a") as f2:
+
+        for character in f1_read:
+
+            if character == " ":
+                f2.write("   ")
+            else:
+                f2.write(character)
+
+        return f2
+
+
+
+
+
 
 
 if __name__ == '__main__':
